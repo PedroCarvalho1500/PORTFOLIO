8cd59f64022c0af26abd9251d4b01f3199b4307f
PedroCarvalho1500 <pedro_alves@gec.inatel.br>
2024-12-04 09:48:50 -0300

    MOD: Responsive images for mobile.

--- scripts/project_preview.js
+++ scripts/project_preview.js
@@ -6,17 +6,17 @@ const url = new URL(current_url);
 const project = url.searchParams.get("project");
 const number_of_files = parseInt(url.searchParams.get("nimg"));
 
-const img_style = `style="width: 120px; height: 900px; padding-left: 10%;padding-right: 10%;"`
+const img_style = `style="padding-left: 10%;padding-right: 10%;"`
 
 const carrosel_active = document.querySelector(`.carousel-item`);
-carrosel_active.innerHTML = `                    <img ${img_style} src="./assets/images/${project}/IMG_1.gif" class="img-carrosel d-block w-100" alt="...">
+carrosel_active.innerHTML = `                    <img ${img_style} src="./assets/images/${project}/IMG_1.gif" class="img-carrosel d-block w-100 img-fluid" alt="...">
                     <div class="carousel-caption d-none d-md-block">
                     </div>`;
 
 
 for(let i=2;i<=number_of_files;i++){
     const new_html = `    <div class="carousel-item" style="background-color: black;">
-        <img ${img_style} src="./assets/images/${project}/IMG_${i}.gif" class="d-block w-100" alt="...">
+        <img ${img_style} src="./assets/images/${project}/IMG_${i}.gif" class="d-block w-100 img-fluid" alt="...">
     </div>`;
 
     document.querySelector(`.carousel-inner`).innerHTML+=new_html;
